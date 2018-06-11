b424060f708285b0878a8c1650b396ebc1bb4493
pototckaia <pototckaia@gmail.com>
2018-06-11 23:56:13 +1000

    test:

--- sort_benchmark.cpp
+++ sort_benchmark.cpp
@@ -3,31 +3,32 @@
 #include "sort.h"
 #include "dump.h"
 
-namespace container_size
-{
-    static std::size_t great = 100000000000000000;
-    static std::size_t big =   100000000000;
-    static std::size_t little = 10000;
-} // container_size
-
 enum sort_mode { SINGLE, MUlTI };
+
 
 static void BM_Sort(benchmark::State& state)
 {
-    state.PauseTiming();
-    auto v = rand_vector(state.range(2));
-    state.ResumeTiming();
+    for (auto _ : state) {
+        state.PauseTiming();
+        auto v = get_test_vector(state.range(1));
+        state.ResumeTiming();
 
-    if (state.range(0) == sort_mode::SINGLE) {
-        tst::sort(v->begin(), v->end());
+        if (state.range(0) == sort_mode::SINGLE) {
+            tst::sort(v->begin(), v->end());
+        }
+        else if (state.range(0) == sort_mode::MUlTI) {
+            tst::multi_sort(v->begin(), v->end(), state.range(2));
+        }
     }
-    else if (state.range(0) == sort_mode::MUlTI) {
-        tst::multi_sort(v->begin(), v->end(), state.range(3) );
-    }
+}
+
+static void CustomTheards(benchmark::internal::Benchmark* b) {
+    for (int i = 1; i <= 16; i*=2)
+        b->Args({sort_mode::MUlTI, container_size::little, i});
 }
 
 BENCHMARK(BM_Sort)
     ->Args({sort_mode::SINGLE, container_size::little})
-    ->Args({sort_mode::MUlTI, container_size::little, 1});
+    ->Apply(CustomTheards);
 
 BENCHMARK_MAIN();
---
+++ sort_test.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include "catch.hpp"
+#include "sort.h"
+#include "dump.h"
+
+auto v_l = get_test_vector(container_size::little);
+
+TEST_CASE("single sort little")
+{
+//    std::vector<int> b = {-966, 5, -10, -15, 20, -15, -25, -1, 0, -966, 50};
+//    tst::sort(b.begin(), b.end());
+//    for (auto p = b.begin(), i = p + 1; i != b.end(); ++i, ++p) {
+//        REQUIRE((*p) <= (*i));
+//    }
+
+    tst::sort(v_l->begin(), v_l->end());
+    for (auto p = v_l->begin(), i = p + 1; i != v_l->end(); ++i, ++p) {
+        REQUIRE((*p) <= (*i));
+    }
+}
+
+TEST_CASE("multi sort little")
+{
+    tst::multi_sort(v_l->begin(), v_l->end(), 10);
+    for (auto p = v_l->begin(), i = p + 1; i !=  v_l->end(); ++i, ++p) {
+        REQUIRE((*p) <= (*i));
+    }
+}
+
+
+auto v_b = get_test_vector(container_size::big);
+
+TEST_CASE("single sort big")
+{
+    tst::sort(v_b->begin(), v_b->end());
+    for (auto p = v_b->begin(), i = p + 1; i != v_b->end(); ++i, ++p) {
+        REQUIRE((*p) <= (*i));
+    }
+}
+
+TEST_CASE("multi sort big")
+{
+    tst::multi_sort(v_b->begin(), v_b->end(), 10);
+    for (auto p = v_b->begin(), i = p + 1; i != v_b->end(); ++i, ++p) {
+        REQUIRE((*p) <= (*i));
+    }
+}
